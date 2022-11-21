0dc73befc3a7fcf7e6b68f25ee88bbd7366f76b0
ssamueluntu <yakusoku751@gmail.com>
2022-11-21 15:23:23 +0800

    tambah login page

--- main.js
+++ main.js
@@ -13,3 +13,27 @@
       window.addEventListener('load', toggleBacktotop)
       onscroll(document, toggleBacktotop)
   }
+
+  // Fungsi autentikasi for login file index
+  function validate() {
+      let username = document.getElementById("username").value;
+      let password = document.getElementById("password").value;
+
+      if (username == "") {
+          alert("Username harus di isi")
+      } else {
+          if (password == "samuel" || password == "Samuel") {
+              this.location.href = "index.html";
+          } else if (password == "Aulya" || password == "aulya") {
+              this.location.href = "index.html";
+          } else if (password == "Holly" || password == "holly") {
+              this.location.href = "index.html";
+          } else if (password == "Marselino" || password == "marselino") {
+              this.location.href = "index.html"
+          } else if (password == null || password == "") {
+              alert("password belum di isi");
+          } else {
+              alert("Password salah");
+          }
+      }
+  };
